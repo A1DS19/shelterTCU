36775d5aa7f9ccd12eaab728ad9c529730d1dba0
A1DS19 <jp06171@gmail.com>
2021-05-15 19:07:26 -0600

    wishlist fix?

--- src/components/profile/Wishlist.tsx
+++ src/components/profile/Wishlist.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Header, Image, Icon, Message, Segment } from 'semantic-ui-react';
+import { Grid, Header, Image, Icon, Message, Segment, Card } from 'semantic-ui-react';
 import { addFavorite, getFavorite } from '../../actions/auth';
 import { StoreState } from '../../reducers';
 import { isMobileOnly } from 'react-device-detect';
@@ -42,48 +42,51 @@ export const Wishlist: React.FC<WishlistProps> = ({}): JSX.Element => {
       <React.Fragment>
         {wishlist?.map((pet) => {
           return (
-            <Segment>
-              <Header>
-                <Image
-                  onClick={() => {
-                    history.push(`/adoption/${pet._id}`);
-                  }}
-                  style={{ cursor: 'pointer' }}
-                  circular
-                  size={isMobileOnly ? 'tiny' : 'small'}
-                  src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
-                />
-              </Header>
-              <Header size='huge'> {toTitleCase(pet.name)}</Header>
-              <Header size='huge'>{pet.location}</Header>
-              <Header size='huge'>{pet.size}</Header>
-
-              {currentUser?.id?.toString() === userId && (
-                <Header
-                  onClick={() => {
-                    dispatch(
-                      addFavorite(
-                        currentUser?.id?.toString() as string,
-                        pet._id as string,
-                        true
-                      )
-                    );
-                  }}
-                  size='huge'
-                  style={{ marginTop: '2rem', cursor: 'pointer' }}
-                >
-                  {currentUser?.wishlist?.find((pett) => pett === pet._id) ? (
-                    <div>
-                      <Icon name='heart' color='orange' />
-                    </div>
-                  ) : (
-                    <div>
-                      <Icon name='heart outline' color='orange' />
-                    </div>
-                  )}
-                </Header>
-              )}
-            </Segment>
+            <Card key={pet._id} fluid>
+              <Image
+                onClick={() => {
+                  history.push(`/adoption/${pet._id}`);
+                }}
+                style={{ cursor: 'pointer' }}
+                className='img-listItem'
+                fluid
+                src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
+              />
+              <Card.Content textAlign='center'>
+                <Card.Header>{toTitleCase(pet.name)}</Card.Header>
+                <Card.Description size='small'>
+                  <strong>Ubicación: </strong>
+                  {pet.location}
+                </Card.Description>
+                <Card.Description size='small'>
+                  <strong>Tamaño:</strong> {pet.size}
+                </Card.Description>
+                {currentUser?.id?.toString() === userId && (
+                  <Card.Description
+                    style={{ cursor: 'pointer', marginTop: '10px' }}
+                    onClick={() => {
+                      dispatch(
+                        addFavorite(
+                          currentUser?.id?.toString() as string,
+                          pet._id as string,
+                          true
+                        )
+                      );
+                    }}
+                  >
+                    {currentUser?.wishlist?.find((pett) => pett === pet._id) ? (
+                      <div>
+                        <Icon name='heart' color='orange' size='large' />
+                      </div>
+                    ) : (
+                      <div>
+                        <Icon name='heart outline' color='orange' size='large' />
+                      </div>
+                    )}
+                  </Card.Description>
+                )}
+              </Card.Content>
+            </Card>
           );
         })}
       </React.Fragment>
@@ -114,7 +117,7 @@ export const Wishlist: React.FC<WishlistProps> = ({}): JSX.Element => {
               src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
             />
 
-            <Header size='huge'> {toTitleCase(pet.name)}</Header>
+            <Header size='huge'>{toTitleCase(pet.name)}</Header>
             <Header size='huge'>{pet.location}</Header>
             <Header size='huge'>{pet.size}</Header>
 
@@ -151,7 +154,35 @@ export const Wishlist: React.FC<WishlistProps> = ({}): JSX.Element => {
 
   return (
     <React.Fragment>
+      <Header content='MASCOTAS FAVORITAS' />
       {isMobileOnly ? renderMobileList() : renderDesktopList()}
     </React.Fragment>
   );
 };
+
+/*
+<Card className='img-container' fluid as={Link} to={`/adoption/${pet._id}`}>
+      <Image
+        className='img-listItem'
+        fluid
+        label={{
+          color: `${JSON.parse(pet.adopted) ? 'red' : 'green'}`,
+          content: `${JSON.parse(pet.adopted) ? 'Adoptado' : 'Disponible'}`,
+          ribbon: 'right',
+        }}
+        src={pet?.photosUrl?.length! > 0 ? pet.photosUrl![0] : '/assets/pet-house.png'}
+      />
+      <Card.Content textAlign='center'>
+        <Card.Header>
+          {' '}
+          <p style={{ color: 'orange' }}>{toTitleCase(pet.name)}</p>{' '}
+        </Card.Header>
+        <Card.Meta>
+          <Icon name='point' /> {pet.location}
+        </Card.Meta>
+        <Card.Description style={{ whiteSpace: 'pre' }}>
+          {pet.description.slice(0, 30) + '...'}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+*/
