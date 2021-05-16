e6fa2f3f233f88d0f4a7a085a6d353ac08a1a3ab
A1DS19 <jp06171@gmail.com>
2021-05-15 18:38:17 -0600

    wishlist fix?

--- src/components/profile/Wishlist.tsx
+++ src/components/profile/Wishlist.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Grid, Header, Image, Icon, Message } from 'semantic-ui-react';
+import { Grid, Header, Image, Icon, Message, Segment } from 'semantic-ui-react';
 import { addFavorite, getFavorite } from '../../actions/auth';
 import { StoreState } from '../../reducers';
 import { isMobileOnly } from 'react-device-detect';
@@ -37,60 +37,121 @@ export const Wishlist: React.FC<WishlistProps> = ({}): JSX.Element => {
     );
   }
 
-  return (
-    <Grid>
-      {wishlist?.map((pet) => (
-        <Grid.Row
-          key={pet._id}
-          style={{
-            display: 'flex',
-            justifyContent: 'space-evenly',
-            backgroundColor: 'white',
-            border: '1px solid gray',
-            margin: '10px 0',
-          }}
-        >
-          <Image
-            onClick={() => {
-              history.push(`/adoption/${pet._id}`);
-            }}
-            style={{ cursor: 'pointer' }}
-            circular
-            size={isMobileOnly ? 'tiny' : 'small'}
-            src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
-          />
+  const renderMobileList = (): JSX.Element => {
+    return (
+      <React.Fragment>
+        {wishlist?.map((pet) => {
+          return (
+            <Segment>
+              <Header>
+                <Image
+                  onClick={() => {
+                    history.push(`/adoption/${pet._id}`);
+                  }}
+                  style={{ cursor: 'pointer' }}
+                  circular
+                  size={isMobileOnly ? 'tiny' : 'small'}
+                  src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
+                />
+              </Header>
+              <Header size='huge'> {toTitleCase(pet.name)}</Header>
+              <Header size='huge'>{pet.location}</Header>
+              <Header size='huge'>{pet.size}</Header>
 
-          <Header size='huge'> {toTitleCase(pet.name)}</Header>
-          <Header size='huge'>{pet.location}</Header>
-          <Header size='huge'>{pet.size}</Header>
-
-          {currentUser?.id?.toString() === userId && (
-            <Header
-              onClick={() => {
-                dispatch(
-                  addFavorite(
-                    currentUser?.id?.toString() as string,
-                    pet._id as string,
-                    true
-                  )
-                );
-              }}
-              size='huge'
-              style={{ marginTop: '2rem', cursor: 'pointer' }}
-            >
-              {currentUser?.wishlist?.find((pett) => pett === pet._id) ? (
-                <div>
-                  <Icon name='heart' color='orange' />
-                </div>
-              ) : (
-                <div>
-                  <Icon name='heart outline' color='orange' />
-                </div>
+              {currentUser?.id?.toString() === userId && (
+                <Header
+                  onClick={() => {
+                    dispatch(
+                      addFavorite(
+                        currentUser?.id?.toString() as string,
+                        pet._id as string,
+                        true
+                      )
+                    );
+                  }}
+                  size='huge'
+                  style={{ marginTop: '2rem', cursor: 'pointer' }}
+                >
+                  {currentUser?.wishlist?.find((pett) => pett === pet._id) ? (
+                    <div>
+                      <Icon name='heart' color='orange' />
+                    </div>
+                  ) : (
+                    <div>
+                      <Icon name='heart outline' color='orange' />
+                    </div>
+                  )}
+                </Header>
               )}
-            </Header>
-          )}
-        </Grid.Row>
-      ))}
-    </Grid>
+            </Segment>
+          );
+        })}
+      </React.Fragment>
+    );
+  };
+
+  const renderDesktopList = (): JSX.Element => {
+    return (
+      <Grid>
+        {wishlist?.map((pet) => (
+          <Grid.Row
+            key={pet._id}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              backgroundColor: 'white',
+              border: '1px solid gray',
+              margin: '10px 0',
+            }}
+          >
+            <Image
+              onClick={() => {
+                history.push(`/adoption/${pet._id}`);
+              }}
+              style={{ cursor: 'pointer' }}
+              circular
+              size={isMobileOnly ? 'tiny' : 'small'}
+              src={pet.photosUrl ? pet.photosUrl[0] : '/public/assets/pawprint.png'}
+            />
+
+            <Header size='huge'> {toTitleCase(pet.name)}</Header>
+            <Header size='huge'>{pet.location}</Header>
+            <Header size='huge'>{pet.size}</Header>
+
+            {currentUser?.id?.toString() === userId && (
+              <Header
+                onClick={() => {
+                  dispatch(
+                    addFavorite(
+                      currentUser?.id?.toString() as string,
+                      pet._id as string,
+                      true
+                    )
+                  );
+                }}
+                size='huge'
+                style={{ marginTop: '2rem', cursor: 'pointer' }}
+              >
+                {currentUser?.wishlist?.find((pett) => pett === pet._id) ? (
+                  <div>
+                    <Icon name='heart' color='orange' />
+                  </div>
+                ) : (
+                  <div>
+                    <Icon name='heart outline' color='orange' />
+                  </div>
+                )}
+              </Header>
+            )}
+          </Grid.Row>
+        ))}
+      </Grid>
+    );
+  };
+
+  return (
+    <React.Fragment>
+      {isMobileOnly ? renderMobileList() : renderDesktopList()}
+    </React.Fragment>
   );
 };
